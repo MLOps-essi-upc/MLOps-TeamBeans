e247da046cc6684fd2c330cabdd97ed4973559a5
mionaD-upc <miona.dimic@estudiantat.upc.edu>
2023-10-17 09:52:18 +0200

    update validate

--- src/features/validate.py
+++ src/features/validate.py
@@ -7,9 +7,9 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 
 parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
 
-sys.path.append(parent_dir)
+sys.path.append(parent_dir+ '/data')
 
-from data import toDF_imgMetadata
+import toDF_imgMetadata
 
 
 train_df, test_df, validation_df = toDF_imgMetadata.main()
